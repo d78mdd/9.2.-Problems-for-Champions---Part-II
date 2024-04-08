eb4bec7d4a6ea835f96ff0c0703fea89ab0aa430
dkostadinov <d78mdd@gmail.com>
2024-04-08 19:30:40 +0300

    Add project files.

---
+++ 01. Passion Days/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+// https://csharp-book.softuni.bg/chapter-09-problems-for-champions-part-2.html#%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0-%D0%B4%D0%BD%D0%B8-%D0%B7%D0%B0-%D1%81%D1%82%D1%80%D0%B0%D1%81%D1%82%D0%BD%D0%BE-%D0%BF%D0%B0%D0%B7%D0%B0%D1%80%D1%83%D0%B2%D0%B0%D0%BD%D0%B5
+namespace _01.Passion_Days
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nachalniPari = double.Parse(Console.ReadLine());
+
+            double nalichniPari = nachalniPari;
+
+            int pokupki = 0;
+
+            string initialCommand;
+            do
+            {
+                initialCommand = Console.ReadLine();
+            }
+            while (initialCommand != "mall.Enter");
+
+            for (; ; )
+            {
+                string buyCommand = Console.ReadLine();
+
+                if (buyCommand == "mall.Exit")
+                {
+                    break;
+                }
+
+                for (int i = 0; i < buyCommand.Length; i++)
+                {
+                    char commandChar = buyCommand[i];
+
+                    double pokupka = 0.0;
+                    if (commandChar >= 'A' && commandChar <= 'Z')
+                    {
+                        double cena = (double)commandChar;
+                        pokupka = cena * 0.5;
+
+                        if (pokupka > nalichniPari)
+                        {
+                            continue;
+                        }
+                        nalichniPari -= pokupka;
+                        pokupki++;
+                    }
+                    else if (commandChar >= 'a' && commandChar <= 'z')
+                    {
+                        double cena = (double)commandChar;
+                        pokupka = cena * 0.3;
+
+                        if (pokupka > nalichniPari)
+                        {
+                            continue;
+                        }
+                        nalichniPari -= pokupka;
+                        pokupki++;
+                    }
+                    else if (commandChar == '%')
+                    {
+                        pokupka = (double)nalichniPari / 2.00;
+
+                        Console.Write(nalichniPari);
+                        Console.Write(" ");
+                        Console.WriteLine(pokupka);
+
+                        if (pokupka > nalichniPari)
+                        {
+                            continue;
+                        }
+                        nalichniPari -= pokupka;
+                        pokupki++;
+                    }
+                    else if (commandChar == '*')
+                    {
+                        double iztegleno = (double)10.00;
+
+                        Console.WriteLine(iztegleno);
+
+                        nalichniPari += iztegleno;
+                    }
+                    else
+                    {
+                        pokupka = (double)commandChar;
+
+                        if (pokupka > nalichniPari)
+                        {
+                            continue;
+                        }
+                        nalichniPari -= pokupka;
+                        pokupki++;
+                    }
+
+                }
+
+            }
+
+            if (pokupki == 0)
+            {
+                Console.WriteLine($"No purchases. Money left: {nalichniPari:F2} lv.");
+            }
+            else
+            {
+                Console.WriteLine($"{pokupki} purchases. Money left: {nalichniPari:F2} lv.");
+            }
+        }
+    }
+}
---
+++ 02. X Expression/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02.X_Expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string izraz = Console.ReadLine();
+
+            foreach (char item in izraz)
+            {
+
+            }
+        }
+    }
+}

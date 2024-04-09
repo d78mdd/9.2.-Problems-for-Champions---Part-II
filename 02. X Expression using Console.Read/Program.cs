54faea1981c0a18ad81e094a7a32816ee2031975
dkostadinov <d78mdd@gmail.com>
2024-04-09 21:53:36 +0300

    initiate 2nd solution

---
+++ 02. X Expression using Console.Read/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace _02.X_Expression_using_Console.Read
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int symbol = Console.Read();
+
+            decimal result = symbol - '0';
+            int expressionOperator = '+';
+
+            while (symbol != '=')
+            {
+                symbol = Console.Read();
+
+                if (symbol == '(')
+                {
+                    
+                    int innerOperator = '+';
+                    symbol = Console.Read();
+                    decimal innerResult = symbol - '0';
+
+
+                    while (symbol != ')')
+                    {
+                        symbol = Console.Read();
+
+                        if (0 <= symbol - '0' && symbol - '0' <= 9)
+                        {
+                            switch (innerOperator)
+                            {
+                                case '+':
+                                    innerResult += symbol - '0';
+                                    break;
+                                case '-':
+                                    innerResult -= symbol - '0';
+                                    break;
+                                case '*':
+                                    innerResult *= symbol - '0';
+                                    break;
+                                case '/':
+                                    innerResult /= symbol - '0';
+                                    break;
+                            }
+
+                        }
+                        else if (symbol == '+' ||
+                            symbol == '-' ||
+                            symbol == '/' ||
+                            symbol == '*')
+                        {
+                            innerOperator = symbol;
+                        }
+
+                    }
+
+                    switch (expressionOperator)
+                    {
+                        case '+':
+                            result += innerResult;
+                            break;
+                        case '-':
+                            result -= innerResult;
+                            break;
+                        case '*':
+                            result *= innerResult;
+                            break;
+                        case '/':
+                            result /= innerResult;
+                            break;
+                    }
+
+                }
+                else if (0 <= symbol - '0' && symbol - '0' <= 9)
+                {
+                    switch (expressionOperator)
+                    {
+                        case '+':
+                            result += symbol - '0';
+                            break;
+                        case '-':
+                            result -= symbol - '0';
+                            break;
+                        case '*':
+                            result *= symbol - '0';
+                            break;
+                        case '/':
+                            result /= symbol - '0';
+                            break;
+                    }
+
+                }
+                else if (symbol == '+' ||
+                    symbol == '-' ||
+                    symbol == '/' ||
+                    symbol == '*')
+                {
+                    expressionOperator = symbol;
+                }
+            }
+
+            Console.WriteLine($"{result:F2}");
+
+        }
+    }
+}
--- 02. X Expression/Program.cs
+++ 02. X Expression/Program.cs
@@ -8,10 +8,124 @@ namespace _02.X_Expression
         {
             string izraz = Console.ReadLine();
 
-            foreach (char item in izraz)
+            double mainResult = 0;
+            double braceResult = 0;
+
+            char mainOperator = 'n';
+            char braceOperator = 'n';
+
+            bool insideBraces = false;
+
+            foreach (char simvol in izraz)
             {
 
+                switch (simvol)
+                {
+                    case '=':
+                        break;
+
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        if (insideBraces)
+                        {
+                            switch (braceOperator)
+                            {
+                                case 'n':
+                                    braceResult = double.Parse(simvol.ToString());
+                                    break;
+                                case '+':
+                                    braceResult += double.Parse(simvol.ToString());
+                                    break;
+                                case '-':
+                                    braceResult -= double.Parse(simvol.ToString());
+                                    break;
+                                case '*':
+                                    braceResult *= double.Parse(simvol.ToString());
+                                    break;
+                                case '/':
+                                    braceResult /= double.Parse(simvol.ToString());
+                                    break;
+                            }
+
+                        }
+                        else  // outside braces
+                        {
+                            switch (mainOperator)
+                            {
+                                case 'n':
+                                    mainResult = double.Parse(simvol.ToString());
+                                    break;
+                                case '+':
+                                    mainResult += double.Parse(simvol.ToString());
+                                    break;
+                                case '-':
+                                    mainResult -= double.Parse(simvol.ToString());
+                                    break;
+                                case '*':
+                                    mainResult *= double.Parse(simvol.ToString());
+                                    break;
+                                case '/':
+                                    mainResult /= double.Parse(simvol.ToString());
+                                    break;
+                            }
+                        }
+                        break;
+
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                        if (insideBraces)
+                        {
+                            braceOperator = simvol;
+
+                        }
+                        else
+                        {
+                            mainOperator = simvol;
+                        }
+                        break;
+
+                    case '(':
+
+                        insideBraces = true;
+                        break;
+                    case ')':
+                        braceOperator = 'n';
+
+                        switch (mainOperator)
+                        {
+                            case 'n':
+                                mainResult = braceResult;
+                                break;
+                            case '+':
+                                mainResult += braceResult;
+                                break;
+                            case '-':
+                                mainResult -= braceResult;
+                                break;
+                            case '*':
+                                mainResult *= braceResult;
+                                break;
+                            case '/':
+                                mainResult /= braceResult;
+                                break;
+                        }
+
+                        insideBraces = false;
+                        break;
+                }
+
             }
+
+            Console.WriteLine($"{mainResult:F2}");
         }
     }
 }
